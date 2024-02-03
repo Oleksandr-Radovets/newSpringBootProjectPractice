5cf7e3427f6fcb33b8876df375d933a9ca454bca
sasha <radovietsoleksandr@gmail.com>
2024-02-04 01:41:47 +0200

    first commit

---
+++ src/main/java/org/example/newspringboootprojectpractice/NewSpringBoootProjectPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.newspringboootprojectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBoootProjectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSpringBoootProjectPracticeApplication.class, args);
+    }
+
+}
